3654564ad1044643678d49c7ecccd61eccec4280
Nihal Borkar <nihalborkar2024.comp@mmcoe.edu.in>
2025-09-25 21:24:16 +0530

    Update Practical3(Load Balancing).cpp

--- Practical3(Load Balancing).cpp
+++ Practical3(Load Balancing).cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char choice;
+    do {
+        int totalServers, requestID;
+        cout << "Enter total servers available: ";
+        cin >> totalServers;
+
+        cout << "Provide the request ID: ";
+        cin >> requestID;
+
+        int result = requestID % totalServers;
+        cout << "Server handling this request: " << result << endl;
+
+        cout << "\nPress 'n' or 'N' to stop, or any other key to continue: ";
+        cin >> choice;
+
+    } while (choice != 'n' && choice != 'N');
+
+    return 0;
+}
+
+
 
